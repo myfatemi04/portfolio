20ed31ec0bc74f99c8e11ab75dea6336bc4e2dc3
Michael Fatemi <myfatemi04@gmail.com>
2022-01-21 01:42:57 -0500

    fix document title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
 import { AppProps } from "next/app";
+import Head from "next/head";
 import "../public/main.scss";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Michael Fatemi</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
