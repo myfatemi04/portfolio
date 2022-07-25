f57eeb7c7afc807df0d3487a1c5cbbdb78bdef13
Michael Fatemi <myfatemi04@gmail.com>
2022-07-25 13:13:38 -0400

    redesigning research section

--- components/Container.tsx
+++ components/Container.tsx
@@ -1,20 +1,16 @@
 import { ReactNode } from "react";
 
-const sidePadding = "3rem";
-const absoluteMinWidth = `calc(100vw - 2 * ${sidePadding})`;
-const minWidth = `min(${absoluteMinWidth}, 30rem)`;
-const maxWidth = `min(${absoluteMinWidth}, 60rem)`;
+// const sidePadding = "3rem";
+// const absoluteMinWidth = `calc(100vw - 2 * ${sidePadding})`;
+// const minWidth = `min(${absoluteMinWidth}, 30rem)`;
+// const maxWidth = `min(${absoluteMinWidth}, 60rem)`;
 
 export default function Container({ children }: { children: ReactNode }) {
   return (
     <div
       style={{
-        margin: "1rem auto",
         display: "flex",
         flexDirection: "column",
-        minWidth,
-        maxWidth,
-        padding: `1rem ${sidePadding}`,
         minHeight: "100vh",
         boxSizing: "border-box",
       }}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,5 @@
-import React from "react";
 import Container from "../components/Container";
 import Youtube from "../components/Youtube";
-
-function Right() {
-  return (
-    <div style={{ float: "right", width: "30%" }}>
-      <img
-        src="/images/rock_centered.jpg"
-        alt="Me, sitting on a rock."
-        style={{
-          borderRadius: "100%",
-          width: "15rem",
-          height: "15rem",
-          objectFit: "cover",
-          padding: "1rem",
-        }}
-      />
-    </div>
-  );
-}
 
 function OtherThingsAboutMe() {
   return (
@@ -116,148 +97,166 @@ function CoolThingsIveMade() {
         <br />
         <a href="http://github.com/myfatemi04/eyeos">Code</a>
       </div>
-      {/* <div className="project-card">
-    <h2>Coronavision</h2>
-  </div> */}
     </>
   );
 }
 
-function ThingsIveWorkedOn() {
+function Experience() {
   return (
-    <>
+    <div style={{ paddingLeft: "4rem" }}>
       <h1>What I've Worked On</h1>
-      <p>
-        <div className="project-card">
+      <div className="project-row">
+        <div className="image">
+          <img src="/images/visium_small.png" alt="Visium" />
+        </div>
+        <div className="content">
           <h2>Dartmouth Machine Learning Intern</h2>
-          <img src="/images/visium_small.png" alt="Visium" width="50%" />
-          <br />
-          As of June 2022, I've been developing machine learning models to make
-          predictions of which genes are most responsible for aggressive cancer
-          tumors as part of the{" "}
-          <a href="https://jlevy44.github.io/levylab/projects/">Levy Lab</a> at
-          the Dartmouth-Hitchcock Medical Center. They may also be useful for
-          determining whether immunotherapy will be effective or not.
+          <em>June 2022 &ndash; Present</em>
+          <p>
+            I've been developing machine learning models to make predictions of
+            which genes are most responsible for aggressive cancer tumors as
+            part of the{" "}
+            <a href="https://jlevy44.github.io/levylab/projects/">Levy Lab</a>{" "}
+            at the Dartmouth-Hitchcock Medical Center. They may also be useful
+            for determining whether immunotherapy will be effective or not.
+          </p>
         </div>
-      </p>
-      <p>
-        <div className="project-card">
+      </div>
+      <div className="project-row">
+        <div className="image">
+          <img src="/images/lasers.JPG" alt="Lasers!" />
+        </div>
+        <div className="content">
           <h2>Army Research Lab Intern</h2>
-          <img src="/images/lasers.JPG" alt="Lasers!" width="50%" />
-          <br />
-          From June 2021 to August 2021, I worked as a research intern for Dr.
-          Ei Brown at the <a href="http://arl.army.mil">Army Research Lab</a> in
-          Adelphi, Maryland to measure the efficiency of various laser crystals
-          by detecting the amount and wavelengths of emitted light from each
-          crystal. I wrote Python programs to automatically detect the decay
-          time of an emitted signal and decompose a single decay signal into
-          various component decay signals. I also wrote a Java program to
-          interface directly with the drivers for a lab spectrometer, setting up
-          a server that piped real-time detection events to clients, performed
-          data transformations such as noise reduction and signal peak
-          identification, and visualized the results in a dashboard.
+          <em>June 2021 &ndash; August 2021</em>
+          <p>
+            I worked as a research intern for Dr. Ei Brown at the{" "}
+            <a href="http://arl.army.mil">Army Research Lab</a> in Adelphi,
+            Maryland to measure the efficiency of various laser crystals by
+            detecting the amount and wavelengths of emitted light from each
+            crystal. I wrote Python programs to automatically detect the decay
+            time of an emitted signal and decompose a single decay signal into
+            various component decay signals. I also wrote a Java program to
+            interface directly with the drivers for a lab spectrometer, setting
+            up a server that piped real-time detection events to clients,
+            performed data transformations such as noise reduction and signal
+            peak identification, and visualized the results in a dashboard.
+          </p>
         </div>
-      </p>
-    </>
+      </div>
+    </div>
   );
 }
 
-function About() {
+function Education() {
   return (
-    <Container>
-      <section>
-        <div style={{ margin: "0px auto", width: "70%" }}>
-          <h1>Education</h1>
-          <div className="edu-row">
-            <div className="image">
-              <img src="/images/uva_logo.png" alt="University of Virginia" />
-            </div>
-            <div className="content">
-              <b>University of Virginia</b>
-              <p>
-                <em>2022 &ndash; 2026</em>
-                <br />
-                BS Computer Science
-              </p>
-            </div>
-          </div>
-          <div className="edu-row">
-            <div className="image">
-              <img
-                src="/images/TJ_logo.png"
-                alt="Thomas Jefferson High School for Science and Technology"
-              />
-            </div>
-            <div className="content">
-              <b>Thomas Jefferson High School for Science and Technology</b>
-              <p>
-                <em>2018 &ndash; 2022</em>
-                <br />
-                Artificial Intelligence, Computer Vision, Mobile and Web App
-                Development, Data Structures
-              </p>
-            </div>
-          </div>
-          <ThingsIveWorkedOn />
-          <CoolThingsIveMade />
-          <h1>Volunteering</h1>
+    <div style={{ paddingLeft: "4rem", maxWidth: "40rem" }}>
+      <h1>Education</h1>
+      <div className="edu-row">
+        <div className="image">
+          <img src="/images/uva_logo.png" alt="University of Virginia" />
+        </div>
+        <div className="content">
+          <b>University of Virginia</b>
           <p>
-            From April 2020 to April 2021, I was a founding member and the
-            director of events at <a href="https://codefycs.org/">Codefy</a>, a
-            computer science tutoring nonprofit. I taught courses in web
-            development and Python, built the enrollment system that was used by
-            thousands of students, and organized workshops in machine learning,
-            game development, and computer vision.
-          </p>
-          <p>
-            As a member of the student government, I{" "}
-            <a href="https://sga.tjhsst.edu/">rebuilt</a> the main website with
-            Typescript and React and built a website{" "}
-            <a href="https://studyguides.sites.tjhsst.edu/login/">
-              where students can share study guides
-            </a>
-            . I also help organize events.
-          </p>
-          <OtherThingsAboutMe />
-          <h1>Contact Me</h1>
-          <p>
-            Please send me an email at gsk6me [at] virginia.edu, or message me
-            on <a href="https://linkedin.com/in/michaelfatemi">LinkedIn</a>.
+            <em>2022 &ndash; 2026</em>
+            <br />
+            BS Computer Science
           </p>
         </div>
-      </section>
-    </Container>
+      </div>
+      <div className="edu-row">
+        <div className="image">
+          <img
+            src="/images/TJ_logo.png"
+            alt="Thomas Jefferson High School for Science and Technology"
+          />
+        </div>
+        <div className="content">
+          <b>Thomas Jefferson High School for Science and Technology</b>
+          <p>
+            <em>2018 &ndash; 2022</em>
+            <br />
+            Artificial Intelligence, Computer Vision, Mobile and Web App
+            Development, Data Structures
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Volunteering() {
+  return (
+    <div>
+      <h1>Volunteering</h1>
+      <p>
+        From April 2020 to April 2021, I was a founding member and the director
+        of events at <a href="https://codefycs.org/">Codefy</a>, a computer
+        science tutoring nonprofit. I taught courses in web development and
+        Python, built the enrollment system that was used by thousands of
+        students, and organized workshops in machine learning, game development,
+        and computer vision.
+      </p>
+      <p>
+        As a member of the student government, I{" "}
+        <a href="https://sga.tjhsst.edu/">rebuilt</a> the main website with
+        Typescript and React and built a website{" "}
+        <a href="https://studyguides.sites.tjhsst.edu/login/">
+          where students can share study guides
+        </a>
+        . I also choreographed a hiphop dances for my year and organized other
+        homecoming events.
+      </p>
+    </div>
+  );
+}
+
+function MainImage() {
+  return (
+    <div className="main-image">
+      <div className="background-pseudo"></div>
+      <div className="main-card">
+        <div style={{ maxWidth: "33vw" }}>
+          <h1>Michael Fatemi</h1>
+
+          <p style={{ display: "inline-block" }}>
+            Hey! I'm Michael Fatemi, a software developer and machine learning
+            enthusiast. I'm interested in biotech, robotics, aerospace, and
+            quantum computing.
+          </p>
+
+          <div className="link-row">
+            <a href="https://github.com/myfatemi04">GitHub</a>
+
+            <a href="https://www.youtube.com/channel/UC0CG_oek4RpaqqJlJlFHSKQ">
+              YouTube
+            </a>
+
+            <a href="https://linkedin.com/in/michaelfatemi">LinkedIn</a>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
 export default function Main() {
   return (
     <>
-      <div className="main-image">
-        <div className="background-pseudo"></div>
-        <div className="main-card">
-          <div style={{ maxWidth: "33vw" }}>
-            <h1>Michael Fatemi</h1>
-
-            <p style={{ display: "inline-block" }}>
-              Hey! I'm Michael Fatemi, a software developer and machine learning
-              enthusiast. I'm interested in biotech, robotics, aerospace, and
-              quantum computing.
-            </p>
-
-            <div className="link-row">
-              <a href="https://github.com/myfatemi04">GitHub</a>
-
-              <a href="https://www.youtube.com/channel/UC0CG_oek4RpaqqJlJlFHSKQ">
-                YouTube
-              </a>
-
-              <a href="https://linkedin.com/in/michaelfatemi">LinkedIn</a>
-            </div>
-          </div>
-        </div>
-      </div>
-      <About />
+      <Container>
+        <MainImage />
+        <Education />
+        <Experience />
+        <CoolThingsIveMade />
+        <Volunteering />
+        <OtherThingsAboutMe />
+        <h1>Contact Me</h1>
+        <p>
+          Please send me an email at gsk6me [at] virginia.edu, or message me on{" "}
+          <a href="https://linkedin.com/in/michaelfatemi">LinkedIn</a>.
+        </p>
+      </Container>
     </>
   );
 }
