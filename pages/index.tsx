00a3a08e8599614764252d5fd75a9d976cf7fce9
Michael Fatemi <myfatemi04@gmail.com>
2023-10-06 22:08:07 -0400

    update personal website - october 2023

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,30 +11,44 @@ function OtherThingsAboutMe() {
         // color: "white",
       }}
     >
-      <h1>Other Things About Me</h1>
-      <div className="project-row">
-        <div className="image">
-          <img src="/images/rubiks_team_2019.jpg" alt="Rubik's Team 2019" />
-        </div>
-        <div className="content">
+      <h1 style={{ textAlign: "center" }}>Other Things About Me</h1>
+
+      <div
+        className="project-grid"
+        style={{ maxWidth: "80rem", margin: "0 auto" }}
+      >
+        <div className="project-quadrant">
           <h2>TJ Rubik's Cube Club - President</h2>
-          <p>
-            This photo is from 2019. We had a lot of fast people in our year,
-            and our team of eight won fastest in the nation for solving 25
-            Rubik's Cubes in 45 seconds!
-          </p>
+          <div className="content">
+            <div className="col">
+              <img
+                src="/images/rubiks_team_2019.jpg"
+                alt="Rubik's Team 2019"
+                style={{ maxWidth: "100%" }}
+              />
+              <p>
+                This photo is from 2019. We had a lot of fast people in our
+                year, and our team of eight won fastest in the nation for
+                solving 25 Rubik's Cubes in 45 seconds!
+              </p>
+            </div>
+          </div>
         </div>
-      </div>
-      <div className="project-row">
-        <div className="image">
-          <img src="/images/track.JPG" alt="Friends from track" />
-        </div>
-        <div className="content">
+        <div className="project-quadrant">
           <h2>Varsity Track - Middle Distance Co-Captain</h2>
-          <p>
-            Some people from the track team and I, after going to regionals in
-            Spring of 2021.
-          </p>
+          <div className="content">
+            <div className="col">
+              <img
+                src="/images/track.JPG"
+                alt="Friends from track"
+                style={{ maxWidth: "100%" }}
+              />
+              <p>
+                Some people from the track team and I, after going to regionals
+                in Spring of 2021.
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -51,8 +65,11 @@ function CoolThingsIveMade() {
         width: "100%",
       }}
     >
-      <h1>Cool Things I've Made</h1>
-      <div className="project-grid">
+      <h1 style={{ textAlign: "center" }}>Cool Things I've Made</h1>
+      <div
+        className="project-grid"
+        style={{ maxWidth: "80rem", margin: "0 auto" }}
+      >
         {/* CCW */}
         <div className="project-quadrant">
           <h2>Cactus Courseware</h2>
@@ -153,128 +170,248 @@ function Experience() {
     <div style={{ paddingLeft: "4rem" }}>
       <h1>Experience</h1>
       <div className="project-row">
-        <div className="image"></div>
+        <h2>
+          Reinforcement Learning Research with University of Maryland and Army
+          Research Lab
+        </h2>
+        <em>May 2023 &ndash; August 2023</em>
         <div className="content">
-          <h2>Kyron Learning</h2>
-          <em>
-            December 2022 &ndash; Present; Natural Language Processing Intern
-          </em>
+          <div className="col">
+            <p>
+              Some existing techniques for robotic path planning use algorithmic
+              optimizers to find the best path. However, as the length and
+              constraints applied to the path become more complex, it becomes
+              impractical to try to find a globally optimal solution.
+              <ul>
+                <li>
+                  To address this, I used reinforcement learning to train a
+                  neural network to generate paths for an agent moving along a
+                  grid with obstacles to increase the amount of ambiguity as to
+                  its final goal.
+                </li>
+                <li>
+                  My approach reduces the time to generate a path by up to 97%
+                  without a significant decrease in path quality. My work is
+                  currently under review at AAMAS 2024.
+                </li>
+              </ul>
+            </p>
+          </div>
+          <div className="col">
+            <img
+              src="/images/arl-robotics-1.png"
+              alt="A visual depicting robotic paths through a graph."
+            />
+            <br />
+            <em>
+              Each path indicates a simulated robotic path through a forest. A
+              few paths are sampled for different levels of ambiguity. Higher
+              ambiguity paths are less efficient but less revealing about future
+              plans.
+            </em>
+          </div>
+        </div>
+      </div>
+      <div className="project-row">
+        <h2>Independent Contracting with Kyron Learning</h2>
+        <em>
+          December 2022 &ndash; October 2023; Natural Language Processing Intern
+        </em>
+        <div className="content">
           <p>
             Developing natural language processing techniques to scale up the
-            world's best teachers. Using large language models and reinforcement
-            learning with human feedback.
-          </p>
-        </div>
-      </div>
-      <div className="project-row">
-        <div className="image"></div>
-        <div className="content">
-          <h2>Solar Car Team</h2>
-          <em>August 2022 &ndash; Present; Embedded Systems</em>
-          <p>
-            Working to develop a solar-powered car. Recently, I contributed to
-            the development of a regenerative braking system to improve the
-            car's battery efficiency.
-          </p>
-        </div>
-      </div>
-      <div className="project-row">
-        <div className="image"></div>
-        <div className="content">
-          <h2>Energy and Light Management Group</h2>
-          <em>October 2022 &ndash; Present</em>
-          <p>
-            Optimizing the training of neuroevolution potentials (NEPs) to
-            predict properties of complex alloys. Using GPUMD and UVA's Rivanna
-            cluster.
-          </p>
-        </div>
-      </div>
-      <div className="project-row">
-        <div className="image">
-          {/* <img src="/images/visium_small.png" alt="Visium" /> */}
-        </div>
-        <div className="content">
-          <h2>Internship with Dartmouth-Hitchcock Medical Center</h2>
-          <em>
-            June 2022 &ndash; Present; with the{" "}
-            <a href="https://jlevy44.github.io/levylab/projects/">Levy Lab</a>
-          </em>
-          <p>
-            Here, the goal is to use spatially-resolved transcriptomics to
-            improve the diagnosis accuracy of cancer metastasis. <br />
-            What I did specifically:
+            world's best teachers. I had a great time exploring meta-prompting
+            techniques. Some of the things I did were:
             <ul>
               <li>
-                Analyzed spatial autocorrelation of colorectal cancer genomics
-                data in Python with SpatialDE and SPARK-X
+                Improve response quality through self-revision, step-by-step
+                reasoning, and automatic prompt optimization.
               </li>
               <li>
-                Developed convolutional neural networks and graph attention
-                networks with Python/Pytorch to predict spatial expression of
-                cancer-related genes with an AUROC of up to 0.89
-              </li>
-            </ul>
-            Also developed some experience with Slurm/high performance
-            computing. Work soon to be submitted to a conference.
-          </p>
-        </div>
-      </div>
-      <div className="project-row">
-        {/* deprecated image field? */}
-        <div className="image"></div>
-        <div className="content">
-          <h2>Sound Camera Project</h2>
-          <em>August 2021 &ndash; May 2022 (student research at TJ)</em>
-          <p>
-            For student research at TJ, I made a "sound camera" with a Raspberry
-            Pi, an embedded webcam, and a microphone array. This used a
-            beamforming algorithm. If you want to read more about how I made it,
-            see{" "}
-            <a href="https://docs.google.com/document/d/1do7sj8hAGTIEzs5tjTuxz3jxNuWoCZ0ob-_WXh10-J8/edit?usp=sharing">
-              here
-            </a>
-            !
-          </p>
-          <img
-            src="/images/soundcamera.png"
-            alt="Sound camera: Hexagonal microphone array with a camera on the top face and a Raspberry Pi below that."
-            style={{ maxWidth: "10rem" }}
-          />
-        </div>
-      </div>
-      <div className="project-row">
-        <div className="image">
-          {/* <img src="/images/lasers.JPG" alt="Lasers!" /> */}
-        </div>
-        <div className="content">
-          <h2>Internship with Army Research Lab</h2>
-          <em>June 2021 &ndash; August 2021; with Dr. Ei Brown</em>
-          <p>
-            My primary research goal was to measure the efficiency of various
-            laser crystals by detecting the amount and wavelengths of emitted
-            light from each crystal. <br />
-            What I did specifically:
-            <ul>
-              <li>
-                Write Python programs to automatically detect the decay time of
-                an emitted signal. This automation reduced analysis time by
-                ~90%.
+                Increased quantitative accuracy by incorporating a code
+                interpreter and WolframAlpha access.
               </li>
               <li>
-                Create a real-time spectrum analyzer that enabled us to discover
-                emissions peaks more efficiently, and decompose a single decay
-                signal into various component decay signals
+                Added the ability for models to render visual aids to help
+                students learn math concepts more effectively than text-only
+                models like GPT-4.
               </li>
             </ul>
           </p>
         </div>
       </div>
       <div className="project-row">
-        <div className="image"></div>
+        <h2>Embedded Systems Development with Solar Car Team</h2>
+        <em>August 2022 &ndash; May 2023; Embedded Systems</em>
         <div className="content">
-          <h2>Project Caelus</h2>
-          <em>November 2020 &ndash; November 2021</em>
+          <div className="col">
+            <p>
+              Working to develop a solar-powered car. Recently, I contributed to
+              the development of a regenerative braking system to improve the
+              car's battery efficiency. I mostly developed embedded software
+              using Mbed OS and C++. I also gained experience using Docker as a
+              simulated build environment.
+            </p>
+          </div>
+          <div className="col">
+            <img
+              src="/images/solar_car.png"
+              alt="A friend and I working to debug an electrical issue."
+            />
+            <br />
+            <em>A friend and I working to debug an electrical issue.</em>
+          </div>
+        </div>
+      </div>
+      <div className="project-row">
+        <h2>
+          Computer Vision Research with Dartmouth-Hitchcock Medical Center
+        </h2>
+        <em>
+          June 2022 &ndash; August 2023; with the{" "}
+          <a href="https://jlevy44.github.io/levylab/projects/">Levy Lab</a>
+        </em>
+        <div className="content">
+          <div className="col">
+            <p>
+              I implemented machine learning models to classify microscope
+              images for the presence of cancer-related genes. I also conducted
+              the majority of the analyses to evaluate the results. This work
+              resulted in three journal papers, two of which are under review,
+              one of which is available here:{" "}
+              <a href="https://www.sciencedirect.com/science/article/pii/S2153353923001220">
+                Inferring spatial transcriptomics markers from whole slide
+                images to characterize metastasis-related spatial heterogeneity
+                of colorectal tumors: A pilot study
+              </a>
+            </p>
+            <p>
+              What I did specifically:
+              <ul>
+                <li>
+                  Analyzed spatial autocorrelation of colorectal cancer genomics
+                  data in Python with SpatialDE and SPARK-X
+                </li>
+                <li>
+                  Developed convolutional neural networks and graph attention
+                  networks with Python/Pytorch to predict spatial expression of
+                  cancer-related genes with an AUROC of up to 0.89
+                </li>
+              </ul>
+            </p>
+          </div>
+        </div>
+        <img
+          src="/images/journal_paper_figure.jpg"
+          alt="A figure illustrating one of the workflows I used."
+          style={{ maxWidth: "40rem" }}
+        />
+        <p>
+          <em>One of the workflows I created.</em>
+        </p>
+      </div>
+      <div className="project-row">
+        <h2>
+          Materials Science Research with Energy and Light Management Group
+        </h2>
+        <em>October 2022 &ndash; February 2023</em>
+        <div className="content">
+          <p>
+            I trained machine learning models to predict the forces between
+            atoms in molecular simulations.
+            <ul>
+              <li>
+                I created a branch of the library{" "}
+                <a href="https://gpumd.org/">GPUMD</a>, which uses Nvidia CUDA
+                to accelerate molecular simulations. My extension added support
+                for deep neural networks of arbitrary size using the CUDA
+                kernel.
+              </li>
+              <li>
+                I implemented custom optimizers for neural networks including
+                gradient descent, genetic algorithms, and simulated annealing.
+              </li>
+            </ul>
+          </p>
+        </div>
+      </div>
+      <div className="project-row">
+        <h2>Sound Camera Project</h2>
+        <em>August 2021 &ndash; May 2022 (student research project)</em>
+        <div className="content">
+          <div className="col">
+            <p>
+              For a year-long student research project in high school, I made a
+              "sound camera" with a Raspberry Pi, an embedded webcam, and a
+              microphone array.
+              <ul>
+                <li>
+                  To do this, I implemented a beamforming algorithm in C and
+                  wrote code to stream information from the microphone array to
+                  a processing server.
+                </li>
+                <li>
+                  Single sound directions of arrival could be distinguished
+                  within 8 degrees of accuracy.
+                </li>
+                <li>
+                  If you want to read more about how I made it, see{" "}
+                  <a href="https://docs.google.com/document/d/1do7sj8hAGTIEzs5tjTuxz3jxNuWoCZ0ob-_WXh10-J8/edit?usp=sharing">
+                    here
+                  </a>
+                  !
+                </li>
+              </ul>
+            </p>
+          </div>
+          <div className="col">
+            <img
+              src="/images/soundcamera.png"
+              alt="Sound camera: Hexagonal microphone array with a camera on the top face and a Raspberry Pi below that."
+            />
+            <br />
+            <em>
+              Hexagonal microphone array with a camera on the top face and a
+              Raspberry Pi below that
+            </em>
+          </div>
+        </div>
+      </div>
+      <div className="project-row">
+        <h2>Internship with Army Research Lab</h2>
+        <em>June 2021 &ndash; August 2021; with Dr. Ei Brown</em>
+        <div className="content">
+          <div className="col">
+            <p>
+              My primary research goal was to measure the efficiency of various
+              laser crystals by detecting the amount and wavelengths of emitted
+              light from each crystal. <br />
+              What I did specifically:
+              <ul>
+                <li>
+                  Write Python programs to automatically detect the decay time
+                  of an emitted signal. This automation reduced analysis time by
+                  ~90%.
+                </li>
+                <li>
+                  Create a real-time spectrum analyzer that enabled us to
+                  discover emissions peaks more efficiently, and decompose a
+                  single decay signal into various component decay signals
+                </li>
+              </ul>
+            </p>
+          </div>
+          <div className="col">
+            <img src="/images/lasers.JPG" alt="Lasers!" />
+            <br />
+            <em>My laser setup.</em>
+          </div>
+        </div>
+      </div>
+      <div className="project-row">
+        <h2>Project Caelus</h2>
+        <em>November 2020 &ndash; November 2021</em>
+        <div className="content">
           <p>
             Project Caelus is aiming to be the first high school team to create
             a liquid-fueled rocket. I wrote C++ code to interface between
@@ -301,8 +438,17 @@ function Education() {
           <b>University of Virginia</b>
           <p>
             <em>2022 &ndash; 2026</em>
-            <br />
-            BS Computer Science
+          </p>
+          <p>Bachelor's of Science in Computer Science and Mathematics</p>
+          <p>
+            Coursework: Advanced Algorithms, Neuroscience, Probability,
+            First-Order Optimization Techniques, Operating Systems, Partial
+            Differential Equations, Complex Variables, Real Analysis
+          </p>
+          <p>
+            Activities: Cavalier Autonomous Racing, Virginia Entrepreneurship
+            Organization, Chinese Student Association, Korean Student
+            Association
           </p>
         </div>
       </div>
@@ -319,8 +465,13 @@ function Education() {
             <em>2018 &ndash; 2022</em>
           </p>
           <p>
-            Artificial Intelligence, Computer Vision, Web/Mobile Development,
-            Data Structures, Macro/Microeconomics, Multivariable Calculus
+            Coursework: Artificial Intelligence, Computer Vision, Web/Mobile
+            Development, Data Structures, Macro/Microeconomics, Multivariable
+            Calculus
+          </p>
+          <p>
+            Activities: Student Government, Rubik's Cube Club, Track and Field,
+            Cross Country, Urban Dance Movement
           </p>
         </div>
       </div>
@@ -391,8 +542,10 @@ export default function Main() {
     <Container>
       <GoogleAnalytics />
       <MainImage />
-      <Education />
-      <Experience />
+      <div style={{ maxWidth: "80rem", margin: "0 auto" }}>
+        <Education />
+        <Experience />
+      </div>
       <Volunteering />
       <CoolThingsIveMade />
       <OtherThingsAboutMe />
