e8aa48c0db474cfad9ad0c9101b9aa80b3b0ec0c
Michael Fatemi <myfatemi04@gmail.com>
2023-10-06 22:38:15 -0400

    migrate to next 18

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,13 @@ import { AppProps } from "next/app";
 import Head from "next/head";
 import "../public/main.scss";
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component }: AppProps) {
   return (
     <>
       <Head>
         <title>Michael Fatemi</title>
       </Head>
-      <Component {...pageProps} />
+      <Component />
     </>
   );
 }
